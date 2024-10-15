1b2940e72a6bc21183d1db03cc08986718d5fcbe
RohanPKulkarni <rohanpkk1818@gmail.com>
2024-10-16 03:13:29 +0530

    dialog control

--- src/app/components/common-card/index.js
+++ src/app/components/common-card/index.js
@@ -14,6 +14,12 @@ export default function Commoncard({name , credits, incharge, linker, code, pyql
     setSelectedCardData({ name, credits, incharge, linker, code , pyqlink, lablink});
     setOpenDialog(true);
   }
+
+  function handledialog(dialoglink){
+    setOpenDialog(false);
+    window.open(dialoglink, "_blank", "noopener,noreferrer");
+    
+  }
   
   return (
     <Card onClick={() => handleCardClick()} className="shadow-sm shadow-red-500 flex flex-col gap-8 rounded-2xl p-2 py-6 transition duration-300 hover:shadow-2xl hover:shadow-gray-600/10 cursor-pointer transform hover:-translate-y-2 bg-transparent border-4 border-black sm:p-8 md:p-8 lg:p-8">
@@ -33,19 +39,19 @@ export default function Commoncard({name , credits, incharge, linker, code, pyql
           </CardDescription>
         </CardHeader>
         <CardFooter className="p-0 justify-between">
-          <Button onClick={() => window.open(pyqlink, "_blank", "noopener,noreferrer")} 
+          <Button onClick={() => handledialog(pyqlink)} 
                   className="custom-shadow ml-3 border-2 border-black bg-transparent hover:bg-transparent text-black hover:text-gray-700 shadow-md transition-all px-4 py-2 rounded-lg font-semibold">
             PYQs
           </Button>
           {lablink ? (
           <Button 
-            onClick={() => window.open(lablink, "_blank", "noopener,noreferrer")} 
+            onClick={() => handledialog(lablink)} 
             className="custom-shadow ml-2 border-2 border-black bg-transparent hover:bg-transparent text-black hover:text-gray-700 shadow-md transition-all px-4 py-2 rounded-lg font-semibold"
           >
             Lab Code
           </Button>
         ) : null}
-          <Button onClick={() => window.open(linker, "_blank", "noopener,noreferrer")} 
+          <Button onClick={() => handledialog(linker)} 
                   className="custom-shadow mr-3 border-2 border-black bg-transparent hover:bg-transparent text-black hover:text-gray-700 shadow-md transition-all px-4 py-2 rounded-lg font-semibold">
             Notes
           </Button>
