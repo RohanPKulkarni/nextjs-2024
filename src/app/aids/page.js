a3f73fe277e1f7a831a7f6bf2448075c6c1614df
RohanPKulkarni <rohanpkk1818@gmail.com>
2024-10-16 02:31:08 +0530

    share card

--- src/app/aids/page.js
+++ src/app/aids/page.js
@@ -2,17 +2,37 @@ import { Semester } from "../components/semesters";
 import { Semdropdown } from "../components/sem-dropdown";
 import { Calender } from "../components/calender";
 
-
+import Head from 'next/head';
 
 export default function Aids(){
 
+  
+
   return (
-    <div className="flex flex-col ">
-      <Semdropdown branch = {"aids"}/>
-      <Semester branch = {"aids"}/>
-      <Calender />
-           
-  </div>
+    <>
+      <Head>
+        <meta property="og:title" content="NoteRit AIML&AIDS" />
+        <meta property="og:description" content="NoteRit-one-stop platform for AIML and AIDS resources." />
+        <meta property="og:image" content="../../../images/ai-image.webp" />
+        <meta property="og:url" content="https://noterit.vercel.app/" />
+        <meta property="og:type" content="website" />
+        
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="NoteRit AIML&AIDS" />
+        <meta name="twitter:description" content="Explore the features and resources available at NoteRit for AIML and AIDS courses." />
+        <meta name="twitter:image" content="https://noterit.vercel.app/" />
+        
+        <title>NoteRit AIML&AIDS</title>
+      </Head>
+          <div className="flex flex-col ">
+          <Semdropdown branch = {"aids"}/>
+          <Semester branch = {"aids"}/>
+          <Calender />
+              
+      </div>
+    </>
+    
+   
     
     
   );
