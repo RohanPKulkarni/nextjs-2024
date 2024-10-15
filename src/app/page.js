551b734e046cc5fc361f4aa4a6ddf062b2486948
RohanPKulkarni <rohanpkk1818@gmail.com>
2024-10-16 01:24:00 +0530

    home page edit

--- src/app/page.js
+++ src/app/page.js
@@ -8,14 +8,14 @@ export default function Home() {
       This platform is designed to provide students of the 
       <span class="font-bold text-red-600"> Artificial Intelligence and Data Science</span> and 
       <span class="font-bold text-red-600"> Artificial Intelligence and Machine Learning</span> branches at M.S. Ramaiah Institute of Technology with a centralized hub for all academic resources. 
-      Here, you'll find class notes, lab codes, and previous years' questions (PYQs) to aid your learning experience. Stay updated 
+      Here, you'll find class notes, lab codes and previous years' questions (PYQs) to aid your learning experience. Stay updated 
       with the latest branch news, important dates, and events through our calendar.
       </p>
       
       
       <p class="text-lg text-black-600 leading-relaxed">
         Students can also contribute by sharing their own notes, fostering a collaborative learning 
-        environment. Together, let's build a strong foundation in AI and beyond.
+        environment. Together, let's build a strong foundation in AI and beyond.Stay tuned, as many more features are on the way!
       </p>
     </section>
   );
