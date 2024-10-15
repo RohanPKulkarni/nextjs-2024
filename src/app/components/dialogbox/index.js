72c3232ef75d748327a894ff492f616982e8c5fd
RohanPKulkarni <rohanpkk1818@gmail.com>
2024-10-15 23:31:22 +0530

    fine tune

--- src/app/components/dialogbox/index.js
+++ src/app/components/dialogbox/index.js
@@ -11,7 +11,7 @@ export default function Dialogbox() {
 
   return (
     <Dialog open = {openDialog} onOpenChange={setOpenDialog}>
-      <DialogContent className=" bg-gradient-to-r from-blue-500 to-green-400 border-4 border-gray-900 sm:max-w-[425px] max-w-[320px] lg:max-w-[500px] dark:bg-gray-800 p-6 rounded-lg shadow-lg">
+      <DialogContent className=" bg-gradient-to-r from-blue-500 to-green-400 border-4 border-gray-900 sm:max-w-[425px] max-w-[340px] lg:max-w-[500px] dark:bg-gray-800 p-6 rounded-lg shadow-lg">
     
       <DialogHeader>
         <DialogTitle className="text-xl font-bold text-gray-900 dark:text-gray-100">{selectedCardData.name}</DialogTitle>
@@ -27,18 +27,18 @@ export default function Dialogbox() {
       
       <div className="mt-6 flex justify-between">
           <Button onClick={() => window.open(selectedCardData.pyqlink, "_blank", "noopener,noreferrer")} 
-                  className="custom-shadow ml-3 border-2 border-black bg-transparent hover:bg-transparent text-black hover:text-gray-700 shadow-md transition-all px-4 py-2 rounded-lg font-semibold focus:outline-none">
+                  className="custom-shadow ml-1 border-2 border-black bg-transparent hover:bg-transparent text-black hover:text-gray-700 shadow-md transition-all px-4 py-2 rounded-lg font-semibold focus:outline-none">
             PYQs
           </Button>
         {selectedCardData.lablink ? (
           <Button onClick={() => window.open(selectedCardData.lablink, "_blank", "noopener,noreferrer")}
-              className="custom-shadow ml-3 border-2 border-black bg-transparent hover:bg-transparent text-black hover:text-gray-700 shadow-md transition-all px-4 py-2 rounded-lg font-semibold focus:outline-none">
+              className="custom-shadow ml-1 border-2 border-black bg-transparent hover:bg-transparent text-black hover:text-gray-700 shadow-md transition-all px-4 py-2 rounded-lg font-semibold focus:outline-none">
                 Lab Code
           </Button>
         ) : null}
         
           <Button onClick={() => window.open(selectedCardData.linker, "_blank", "noopener,noreferrer")}
-                  className="custom-shadow mr-3 border-2 border-black bg-transparent hover:bg-transparent text-black hover:text-gray-700 shadow-md transition-all px-4 py-2 rounded-lg font-semibold focus:outline-none">
+                  className="custom-shadow mr-2 border-2 border-black bg-transparent hover:bg-transparent text-black hover:text-gray-700 shadow-md transition-all px-4 py-2 rounded-lg font-semibold focus:outline-none">
             Notes
           </Button>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <section class="max-w-3xl bg-transparent mx-auto p-8 custom-shadow1 rounded-lg mt-12 hover:shadow-xl transition-shadow duration-300 border-2 border-black ">
+    <section class="max-w-3xl bg-transparent mx-auto p-8 lg:custom-shadow1 rounded-lg mt-12 hover:shadow-xl transition-shadow duration-300 border-2 border-black ">
       <h1 class="text-4xl font-bold text-center text-gray-800 mb-6">Welcome to the AI Branch Portal</h1>
       
       <p class="text-lg text-black-600 mb-4 leading-relaxed">
