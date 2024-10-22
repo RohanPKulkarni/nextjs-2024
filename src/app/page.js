1839bcdab5c8ecc5ceaa0fbfa64f3f01be25cfd0
RohanPKulkarni <rohanpkk1818@gmail.com>
2024-10-22 23:50:48 +0530

    paddingreduced

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
 export default function Home() {
   return (
-    <section className="max-w-3xl bg-transparent mx-auto p-8 lg:custom-shadow1 rounded-lg mt-6 lg:mt-12 md:mt-8 hover:shadow-xl transition-shadow duration-300 border-2 border-black ">
+    <section className="max-w-3xl bg-transparent mx-auto p-4 lg:custom-shadow1 rounded-lg mt-6 lg:mt-12 md:mt-8 hover:shadow-xl transition-shadow duration-300 border-2 border-black ">
       <h1 class="text-4xl font-bold text-center text-gray-800 mb-6">
         Welcome to <span class="text-red-700">NoteRit</span> AI Branch Portal
       </h1>
