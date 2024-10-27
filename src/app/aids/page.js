0c2c9ad7855ee6d19137db893650ad6e2f87e2d1
RohanPKulkarni <rohanpkk1818@gmail.com>
2024-10-28 01:20:35 +0530

    sharecard

--- src/app/aids/page.js
+++ src/app/aids/page.js
@@ -3,28 +3,13 @@ import { Semdropdown } from "../components/sem-dropdown";
 import { Calender } from "../components/calender";
 import { DialogImage } from "../components/dialogimage";
 
-import Head from 'next/head';
 
 export default function Aids(){
 
-  
 
   return (
-    <>
-      <Head>
-        <meta property="og:title" content="NoteRit AIML&AIDS" />
-        <meta property="og:description" content="NoteRit-one-stop platform for AIML and AIDS resources." />
-        <meta property="og:image" content="https://noterit.vercel.app/images/ai-image.webp" />
-        <meta property="og:url" content="https://noterit.vercel.app/" />
-        <meta property="og:type" content="website" />
-        
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="NoteRit AIML&AIDS" />
-        <meta name="twitter:description" content="Explore the features and resources available at NoteRit for AIML and AIDS courses." />
-        <meta name="twitter:image" content="https://noterit.vercel.app/images/ai-image.webp" />
-        
-        <title>NoteRit AIML&AIDS</title>
-      </Head>
+  
+      
           <div className="flex flex-col ">
           <Semdropdown branch = {"aids"}/>
           <Semester branch = {"aids"}/>
@@ -32,7 +17,7 @@ export default function Aids(){
           <DialogImage/>
               
       </div>
-    </>
+  
     
    
     
