d5725422503768135d598a487cddcf3cbbf3013c
RohanPKulkarni <rohanpkk1818@gmail.com>
2024-11-04 22:15:04 +0530

    url added

--- src/app/components/common-layout/index.js
+++ src/app/components/common-layout/index.js
@@ -2,7 +2,7 @@ import Header from "../header";
 
 async function fetchListOfAidsSems() {
   try {
-    const apiResponse = await fetch("/api/getaids-sem", {
+    const apiResponse = await fetch("https://noterit.vercel.app/api/getaids-sem", {
       method: "GET",
     });
 
@@ -16,7 +16,7 @@ async function fetchListOfAidsSems() {
 
 async function fetchListOfAimlSems() {
   try {
-    const apiResponse = await fetch("/api/getaiml-sem", {
+    const apiResponse = await fetch("https://noterit.vercel.app/api/getaiml-sem", {
       method: "GET",
     });
 
