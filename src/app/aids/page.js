28199607331c4a59f2d35df702ec21a50d8c8b6d
RohanPKulkarni <rohanpkk1818@gmail.com>
2024-11-04 23:15:12 +0530

    remove cache

--- src/app/aids/page.js
+++ src/app/aids/page.js
@@ -8,7 +8,6 @@ async function fetchListOfSems() {
   try {
     const apiResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getaids-sem`, {
       method: "GET",
-      cache:"no-store"
     });
 
     const result = await apiResponse.json();
