96bd7d317152a261c4992e4a8d7157696ac7ee4d
RohanPKulkarni <rohanpk1818@gmail.com>
2024-11-19 18:55:57 +0530

    textlg

--- src/app/components/branchcard/index.js
+++ src/app/components/branchcard/index.js
@@ -22,11 +22,11 @@ export default function Branchcard(){
   return(
     <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-y-4 lg:gap-4">
       <Button onClick={() => handlecardclick("aids")}
-              className="w-[90%] px-6 py-4 text-xs md:text-md font-semibold text-white bg-blue-600 rounded-lg border-2 border-white shadow-lg hover:bg-blue-800 hover:shadow-xl transition duration-300 focus:outline-none focus:ring-4 focus:ring-blue-400 focus:ring-offset-2 mx-auto">
+              className="w-[90%] px-6 py-4 text-xs md:text-md lg:text-lg font-semibold text-white bg-blue-600 rounded-lg border-2 border-white shadow-lg hover:bg-blue-800 hover:shadow-xl transition duration-300 focus:outline-none focus:ring-4 focus:ring-blue-400 focus:ring-offset-2 mx-auto">
         Artificial Intelligence and Data Science
       </Button>
       <Button onClick={() => handlecardclick("aiml")}
-              className="w-[90%] px-6 py-4 text-xs md:text-md font-semibold text-white bg-blue-600 rounded-lg border-2 border-white shadow-lg hover:bg-blue-800 hover:shadow-xl transition duration-300 focus:outline-none focus:ring-4 focus:ring-blue-400 focus:ring-offset-2 mx-auto">
+              className="w-[90%] px-6 py-4 text-xs md:text-md lg:text-lg font-semibold text-white bg-blue-600 rounded-lg border-2 border-white shadow-lg hover:bg-blue-800 hover:shadow-xl transition duration-300 focus:outline-none focus:ring-4 focus:ring-blue-400 focus:ring-offset-2 mx-auto">
         Artificial Intelligence and Machine Learning
       </Button>
     </div>
