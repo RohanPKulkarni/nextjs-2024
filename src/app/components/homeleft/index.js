a86703cbcd5b75174b0b786583424b64cbec3070
RohanPKulkarni <rohanpk1818@gmail.com>
2024-11-21 22:47:13 +0530

    smallscreen

--- src/app/components/homeleft/index.js
+++ src/app/components/homeleft/index.js
@@ -126,7 +126,7 @@ export default function TextAnimation({aidssemesters,aimlsemesters}) {
         />
       </div>
 
-      <div className="relative flex flex-col items-center lg:items-start ml-12 sm:ml-44 lg:ml-16">
+      <div className="relative flex flex-col items-center lg:items-start ml-16 sm:ml-44 lg:ml-16">
     <div className="relative w-48 sm:w-[330px] md:w-[360px] lg:w-[380px]">
       <Input
         type="text"
