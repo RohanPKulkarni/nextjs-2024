3199d972c00ec63767fec4e03928b9a6f85fe4ce
RohanPKulkarni <rohanpk1818@gmail.com>
2024-12-05 22:02:00 +0530

    contact

--- src/app/contribute/page.js
+++ src/app/contribute/page.js
@@ -3,9 +3,17 @@ import Installation from "../components/installation";
 export default function Soon() {
   return (
     <div className="flex flex-col items-center p-6 bg-transparent">
-    <h1 className="text-2xl font-bold text-white">Contact Us</h1>
-    <p className="mt-2 text-lg text-white">Feel free to reach out via email:</p>
+    <h1 className="text-2xl font-bold text-black">Contact Us</h1>
+    <p className="mt-2 text-lg text-black">Feel free to reach out via email:</p>
     <div className="mt-4 space-y-2 flex flex-col">
+      <p className="mt-2 text-lg text-black">2nd year :</p>
+      <a 
+        href="mailto:ayushjain191105@gmail.com" 
+        className="text-lg font-semibold text-red-600 hover:underline"
+      >
+        ayushjain191105@gmail.com
+      </a>
+      <p className="mt-2 text-lg text-black">3rd year :</p>
       <a 
         href="mailto:chinmayanarmdeshwar@gmail.com" 
         className="text-lg font-semibold text-red-600 hover:underline"
