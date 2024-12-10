381b90e04c646c282a07745df8aad611c7294028
RohanPKulkarni <rohanpk1818@gmail.com>
2024-12-10 18:52:19 +0530

    added 1st year

--- src/app/contribute/page.js
+++ src/app/contribute/page.js
@@ -6,6 +6,13 @@ export default function Soon() {
     <h1 className="text-2xl font-bold text-black">Contact Us</h1>
     <p className="mt-2 text-lg text-black">Feel free to reach out via email:</p>
     <div className="mt-4 space-y-2 flex flex-col">
+    <p className="mt-2 text-lg text-black">1st year :</p>
+      <a 
+        href="mailto:kamalapurkar.3655@gmail.com" 
+        className="text-lg font-semibold text-red-600 hover:underline"
+      >
+        kamalapurkar.3655@gmail.com
+      </a>
       <p className="mt-2 text-lg text-black">2nd year :</p>
       <a 
         href="mailto:ayushjain191105@gmail.com" 
